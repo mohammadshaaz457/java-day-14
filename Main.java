f2cb7acc1d50cc2117da1ef359b7cfb55a8af01c
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-15 10:41:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        Runnable r =()->System.out.println("i am new from JAVA8 version");
+        Thread t =new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+
+    }
+}
